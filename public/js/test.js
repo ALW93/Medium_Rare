2483f468a1640396d611ead40c9380048b8fd679
alycialwang@gmail.com <alycialwang@gmail.com>
2021-01-05 13:40:03 -0500

    New Article Form Foundation Done

--- public/js/test.js
+++ public/js/test.js
@@ -19,19 +19,20 @@ document.getElementById("image_fetch").addEventListener("click", async (e) => {
   e.preventDefault();
   const search = document.getElementById("Cover__Search").value;
   const request = `https://source.unsplash.com/random/1200x550/?${search}`;
+  const url = await fetch(request).then((data) => data.url);
   const container = document.getElementById("article__background");
-  container.setAttribute("style", `background-image: url(${request})`);
-  container.setAttribute("value", request);
+  container.setAttribute("style", `background-image: url(${url})`);
+  container.setAttribute("value", url);
 });
 
-document
-  .getElementById("article__background")
-  .addEventListener("click", (e) => {
-    console.log("something", e.target.getAttribute("value"));
-  });
+// *** Submit Handler for Article Form Data ***
 document.getElementById("article__submit").addEventListener("click", (e) => {
   e.preventDefault();
-  var delta = quill.getContents();
-  console.log(delta);
-  console.log(quill.root.innerHTML);
+  const title = document.getElementById("article__title").value;
+  const cover = document
+    .getElementById("article__background")
+    .getAttribute("value");
+  const body = quill.root.innerHTML;
+  const delta = quill.getContents();
+  console.log(title, cover, body, delta);
 });
